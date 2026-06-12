e88f4dcc63fc751228601d82e1c18bbecef6864b
alex-bigbattery <alex.g@bigbattery.com>
2026-06-12 14:38:23 -0500

    Update sync.js

--- sync.js
+++ sync.js
@@ -6,6 +6,7 @@ config()
 
 const BASE = 'https://bigbattery.com/wp-json/affwp/v1'
 const AUTH = Buffer.from(
+
   `${process.env.AFFWP_PUBLIC_KEY}:${process.env.AFFWP_TOKEN || process.env.AFFWP_SECRET_KEY}`
 ).toString('base64')
 const sleep = ms => new Promise(r => setTimeout(r, ms))
